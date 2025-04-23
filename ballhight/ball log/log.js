c71f8b772c14779f1ff50862832ae3590de63a23
Um1rbek <farxodovumar2@gmail.com>
2025-04-23 22:20:38 +0500

    initial commiy

--- ballhight/ball log/log.js
+++ ballhight/ball log/log.js
@@ -11,7 +11,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
       localStorage.setItem('username', username);
   
       // Sahifani welcome.html ga yo'naltirish
-      window.location.href = '../ballhight/ball.html';
+      window.location.href = '../ball.html';
     } else {
       document.getElementById('errorMsg').textContent = 'Incorrect password!';
     }
